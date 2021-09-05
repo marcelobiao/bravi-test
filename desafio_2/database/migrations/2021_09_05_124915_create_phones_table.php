e8f5ab1e57a17a3be409a5b2c793c61661ce4e27
Marcelo Bião <marcelobiao@assistevida.com>
2021-09-05 10:03:18 -0300

    Implementando migrations das entidades people, phone e email

---
+++ desafio_2/database/migrations/2021_09_05_124915_create_phones_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->integer('number');
+            $table->unsignedInteger('people_id');
+            $table->foreign('people_id')
+                ->references('id')
+                ->on('people')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
---
+++ desafio_2/database/migrations/2021_09_05_124935_create_emails_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->string('email');
+            $table->unsignedInteger('people_id');
+            $table->foreign('people_id')
+                ->references('id')
+                ->on('people')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
